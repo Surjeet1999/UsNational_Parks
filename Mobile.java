f76b5b4611cec4ec21337767978e8658d4bf4a61
unknown <surjeetrawat246@gmail.com>
2021-05-16 17:05:27 +0530

    Mobile.java

--- Mobile.java
+++ Mobile.java
@@ -1,7 +1,10 @@
-class Mobile{
-    public static void main(String ...args)
-    {
+class Mobile {
+    public static void main(String... args) {
         System.out.println("This is the Mobile class");
+     
+    } 
+    
+    public void m(){
+        Systetm.out.println("This is the m method of the Mobile class");
     }
-    public void m(){}
 }
